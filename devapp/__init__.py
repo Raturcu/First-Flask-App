51ed79f2b37cd465a21d0360f3b8cb5f9192be45
Raturcu <razvan.andrei.turcu97@gmail.com>
2023-02-02 15:00:55 +0200

    auth

--- devapp/__init__.py
+++ devapp/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 def create_app():
     app=Flask(__name__)
+    app.config['SECRET_KEY']='asdljhkh qlweqoiweu'
     
     from .views import views
     from .auth import auth
--- devapp/auth.py
+++ devapp/auth.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-from flask import render_template
+from flask import render_template,request
+from flask import flash
 
-auth = Blueprint('authh', __name__)
+auth = Blueprint('auth', __name__)
 
 @auth.route('/')
 def home():
@@ -9,6 +10,24 @@ def home():
 
 
 
-@auth.route('/register')
+@auth.route('/register',methods=['GET','POST'])
 def register():
+    if request.method=='POST':
+        username=request.form.get('username')
+        email=request.form.get('email')
+        password1=request.form.get('password1')
+        password2=request.form.get('password2')
+        
+        if len(str(username)) < 3:
+            flash("Username must contains at least 4 characters!",category='error')
+        elif len(str(email)) < 5:
+            flash("Email adress must contains at least 5 characters!",category='error')
+        elif password1 != password2:
+            flash("Password don't match",category='error')
+        elif len(str(password1))< 8:
+            flash("Password must be at least 8 characters",category='error')
+        else:
+            flash('Account Created!',category='success')
+        
+   
     return render_template('register.html')
